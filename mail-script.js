1ed365b5465c4fb9487c357d13fc460b698534fe
karjren <vercalcaben@gmail.com>
2024-09-01 08:54:00 +0800

    Create mail-script.js

---
+++ mail-script.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function($) {
+  $('#contact-form').submit(function(event) {
+	var spin = document.getElementById("form-loading");
+	var load = document.getElementById("form-loading-wrapper");
+    load.style.display = "flex";
+    event.preventDefault();  
+    $.ajax({
+      type: 'POST',
+      url: myAjax.ajaxurl,
+      data: {
+        action: 'send_contact_form',
+        name: $('#name').val(),
+        email: $('#email').val(),
+        message: $('#message').val()
+      },
+      success: function(response) {
+		spin.style.display = "none";
+        $('#form-response').html(response);
+      },
+            error: function() {
+                $('#form-response').html(response);
+            }
+    });
+  });
+ $('#inquiry').submit(function(event) {
+	var spin = document.getElementById("form-loading");
+	var load = document.getElementById("form-loading-wrapper");
+    load.style.display = "flex";
+    event.preventDefault();  
+    $.ajax({
+      type: 'POST',
+      url: myAjax.ajaxurl,
+      data: {
+        action: 'send_inquire_form',
+        name: $('#booking-name').val(),
+        email: $('#booking-email').val(),
+        vehicle_type: $('#vehicle-type').val(),
+		deliver: $('#deliver').val(),
+        deldate: $('#deldate').val(),
+		deltime: $('#deltime').val(),
+        return: $('#return').val(),
+		retdate: $('#retdate').val(),
+        rettime: $('#rettime').val()
+      },
+      success: function(response) {
+		spin.style.display = "none";
+        $('#form-response').html(response);
+      },
+            error: function() {
+                $('#form-response').html(response);
+            }
+    });	
+ });
+});
